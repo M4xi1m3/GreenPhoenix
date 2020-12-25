f227280ec72a1563639367caf3214eb5c1905334
M4x1m3 <M4x1me@protonmail.com>
2020-12-25 11:25:44 +0100

    Basic build system

---
+++ gpnet/src/test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int suus(int a, int b) {
+    return a*b;
+}
+
---
+++ gpserver/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "test.h"
+
+int main(int argc, char** argv) {
+    std::cout << suus(4,5) << std::endl;
+}
+
