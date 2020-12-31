8b7d0aafce2581c9161961addd949081288454d6
M4x1m3 <M4x1me@protonmail.com>
2020-12-31 16:27:54 +0100

    Added pimp and upload binaries

--- gpproxy/src/main.cpp
+++ gpproxy/src/main.cpp
@@ -7,7 +7,21 @@
 #include "gpproxy/ProxyConfig.hpp"
 #include "gpproxy/Proxy.hpp"
 
+void printIntro() {
+    std::cout << std::endl;
+    std::cout << "     ,,,     ,,                                                                       " << std::endl;
+    std::cout << "   .;;'        ';      _____                     _____  _                      _      " << std::endl;
+    std::cout << "  /((,  ,//;'`  ,,    / ____|                   |  __ \\| |                    (_)     " << std::endl;
+    std::cout << "  (((( ,;//     ,,.  | |  __ _ __ ___  ___ _ __ | |__) | |__   ___   ___ _ __  ___  __" << std::endl;
+    std::cout << "  (((( ;///\\   ,,,,  | | |_ | '__/ _ \\/ _ \\ '_ \\|  ___/| '_ \\ / _ \\ / _ \\ '_ \\| \\ \\/ /" << std::endl;
+    std::cout << "  \\(((////////,,,,   | |__| | | |  __/  __/ | | | |    | | | | (_) |  __/ | | | |>  < " << std::endl;
+    std::cout << "   `((////////;;,     \\_____|_|  \\___|\\___|_| |_|_|    |_| |_|\\___/ \\___|_| |_|_/_/\\_\\" << std::endl;
+    std::cout << "      ``'^^^'``                                         - Proxy v1.0.0 - AGPLv3       " << std::endl;
+    std::cout << std::endl;
+}
+
 int main(int argc, char** argv) {
+    printIntro();
     stde::net::init();
 
     gp::proxy::Proxy::getInstance().run();
